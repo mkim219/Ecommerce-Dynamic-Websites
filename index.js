c1cd728e381dcb32669142001ae52fd6901cac95
Minsu Kim <51520671+mkim219@users.noreply.github.com>
2020-02-20 23:39:33 -0500

    Add validation

--- index.js
+++ index.js
@@ -40,64 +40,61 @@ app.get("/customer-registration", (req, res) => {
 });
 
 app.post("/customer-registration", (req, res) => {
-
     const errorMessagesFullName = [];
     const errorMessagesEmail = [];
     const errorMessagesPassword = [];
     const errorMessagesRe = [];
+    const errorValLen = [];
+    const errors = {};
 
     //validation
-    if(req.body.fullName=="")
+    if(!req.body.fullName)
     {
-        errorMessagesFullName.push("You must enter your name");
+        errors.errorName = ["You must enter your name"]
+       // errorMessagesFullName.push("You must enter your name");
     }
 
     if(req.body.email=="")
     {
-        errorMessagesEmail.push("You must enter your email");
+        errors.errorEmail = ["You must enter your email"];
     }
 
     if(req.body.psw=="")
     {
-        errorMessagesPassword.push("You must enter your password");
+        errors.errorPws = ["You must enter your password"];
     }
 
     if(req.body.pswrepeat=="")
     {
-        errorMessagesRe.push("You must enter your password again");
+        errors.errorRe = ["You must enter your password again"];
     }
-
-
-    //If the user does not enter all the information
-    if(errorMessagesFullName.length > 0 || 
-       errorMessagesEmail.length > 0 || 
-       errorMessagesPassword.length > 0 || 
-       errorMessagesRe.length > 0)
-    {
-            res.render("customer-registration",{
-                    error0 : errorMessagesFullName,
-                    error1 :errorMessagesEmail,
-                    error2 : errorMessagesPassword,
-                    error3 : errorMessagesRe
-            });
+    if(req.body.psw.length < 9 || req.body.psw.length > 12){
+        errors.errorLen = ["You must enter password length bewteen 9 to 12"];
     }
+   
+    //If theres any errors in the error object, reject the registration and display validation
+    if (Object.keys(errors).length) {
+        res.render("customer-registration", errors);
+        
+    } 
 
 
-    //If the user enters all the data and submit the form
-    else
-    {
-            //descturing
-            const {fullName} =req.body;
-            res.render("customer-registration",{
-                    successMessage :`Thank you ${fullName}
-                    we received your information and will contact you shortly`
-            });
+    //record the customer information
 
-    }
-       
-    
+
+    //send email
+
+
+    // //If the user enters all the data and submit the form
+    // const { fullName } = req.body;
+    // res.render("customer-registration",{
+    //     successMessage :`Thank you ${fullName}
+    //     we received your information and will contact you shortly`
+    // });
 
 });
+
+
 
 app.get("/login", (req, res) => {
 
@@ -119,7 +116,7 @@ app.post("/login", (req, res) => {
 
 });
 
-const PORT= process.env.PORT ||3000;
+const PORT= process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Web Server Started`);
 });
