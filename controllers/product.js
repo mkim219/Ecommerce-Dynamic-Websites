52ca6a0a4909c0ab46ecb526722c0da0632e8e8a
Minsu Kim <51520671+mkim219@users.noreply.github.com>
2020-04-13 21:21:20 -0400

    Fix broken image when re-upload image in proudct Edit

--- controllers/product.js
+++ controllers/product.js
@@ -10,27 +10,27 @@ const addition = require("../model/product");
 router.get("/products", (req, res) => {
 
     addition.find()
-    .then((products)=>{
-        const product_List = products.map(products=>{
-            return { 
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
-            }
-        }); 
-        res.render("products",{
-            data: product_List,
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+        .then((products) => {
+            const product_List = products.map(products => {
+                return {
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+                }
+            });
+            res.render("products", {
+                data: product_List,
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 });
 
-router.post("/productAdd",isAuthenticated, checkAdmin,(req, res) => {
+router.post("/productAdd", isAuthenticated, checkAdmin, (req, res) => {
     const newProduct = {
         pname: req.body.pname,
         pprice: req.body.pprice,
@@ -41,225 +41,236 @@ router.post("/productAdd",isAuthenticated, checkAdmin,(req, res) => {
     }
     const addProduct = new addition(newProduct);
     addProduct.save()
-    .then((pic)=>{
 
-        req.files.productPic.name = `pro_pic_${pic._id}${path.parse(req.files.productPic.name).ext}`;
+        .then((pic) => {
+            req.files.productPic.name = `pro_pic_${pic._id}${path.parse(req.files.productPic.name).ext}`;
+            req.files.productPic.mv(`public/uploads/${req.files.productPic.name}`)
+                .then(() => {
+                    addition.updateOne({ _id: pic._id }, {
+                        productPic: req.files.productPic.name
+                    })
+                        .then(() => {
+                            res.redirect(`/productAdd`)
+                        })
 
-        req.files.productPic.mv(`public/uploads/${req.files.productPic.name}`)
-        .then(()=>{
+                })
+        }).catch(err => console.log(`Error happended when inserting data into database ${err}`))
 
-
-            addition.updateOne({_id:pic._id},{
-                productPic: req.files.productPic.name
-            })
-            .then(()=>{
-                res.redirect(`/productAdd`)
-            })
-
-        })
-      
-      
-       
-    }).catch(err => console.log(`Error happended when inserting data into database ${err}`))
-    
 });
 
-router.get("/productAdd",isAuthenticated, checkAdmin,(req, res) => {
- 
+router.get("/productAdd", isAuthenticated, checkAdmin, (req, res) => {
+
     res.render("productAdd", {
     });
 
 });
 
 router.get("/belt", (req, res) => {
-    addition.find({type: "Belt"})
-    .then((products)=>{
-        const product_List = products.map(products=>{
-            return { 
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
-            }
-        }); 
-        res.render("belt",{
-            belt: product_List
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+    addition.find({ type: "Belt" })
+        .then((products) => {
+            const product_List = products.map(products => {
+                return {
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+                }
+            });
+            res.render("belt", {
+                belt: product_List
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 });
 
 router.get("/shoes", (req, res) => {
-    addition.find({type: "Lifting Shoes"})
-    .then((products)=>{
-       
-        const product_List = products.map(products=>{
-            return { 
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
-            }
-        });
-        res.render("shoes",{
-            shoes: product_List
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+    addition.find({ type: "Lifting Shoes" })
+        .then((products) => {
+
+            const product_List = products.map(products => {
+                return {
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+                }
+            });
+            res.render("shoes", {
+                shoes: product_List
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 
 });
 
 router.get("/sleeves", (req, res) => {
-    addition.find({type: "Sleeves"})
-    .then((products)=>{
-    
-        const product_List = products.map(products=>{
-            return { 
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
-            }
-        }); 
-        res.render("sleeves",{
-            sleeves: product_List
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+    addition.find({ type: "Sleeves" })
+        .then((products) => {
+
+            const product_List = products.map(products => {
+                return {
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+                }
+            });
+            res.render("sleeves", {
+                sleeves: product_List
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 });
 
 router.get("/wrist", (req, res) => {
-    addition.find({type: "Wrist Wrap"})
-    .then((products)=>{
-    
-        const product_List = products.map(products=>{
-            return { 
-                
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
+    addition.find({ type: "Wrist Wrap" })
+        .then((products) => {
 
-            }
-        }); 
-        res.render("wrist",{
-            wrist: product_List
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+            const product_List = products.map(products => {
+                return {
+
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+
+                }
+            });
+            res.render("wrist", {
+                wrist: product_List
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 
 });
 
 
-router.get("/productDash",isAuthenticated, checkAdmin,(req, res) => {
- 
+router.get("/productDash", isAuthenticated, checkAdmin, (req, res) => {
+
     addition.find()
-    .then((products)=>{
-        
-        const product_List = products.map(products=>{
-            return { 
-                id: products._id,
-                pname: products.pname,
-                pprice: products.pprice,
-                type: products.type,
-                pquan: products.pquan,
-                isBest: products.isBest,
-                pdet: products.pdet,
-                pic: products.productPic
-            }
-        }); 
-     
-        res.render("productDash",{
-            data: product_List,
-        });
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+        .then((products) => {
+
+            const product_List = products.map(products => {
+                return {
+                    id: products._id,
+                    pname: products.pname,
+                    pprice: products.pprice,
+                    type: products.type,
+                    pquan: products.pquan,
+                    isBest: products.isBest,
+                    pdet: products.pdet,
+                    pic: products.productPic
+                }
+            });
+
+            res.render("productDash", {
+                data: product_List,
+            });
+        })
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 
 });
 
-router.get("/productEdit/:id",isAuthenticated, checkAdmin,(req,res)=>{
+router.get("/productEdit/:id", isAuthenticated, checkAdmin, (req, res) => {
+
 
     addition.findById(req.params.id) // return an array when using find() method . use the find when you want to pull mutiple values from database 
-    .then((editProduct)=>{
-        const {_id,pname,pprice,type,pquan,isBest,pdet} = editProduct; // destructing object
-        res.render("productEdit",{
-            _id,
-            pname,
-            pprice,
-            type,
-            pquan,
-            isBest,
-            pdet
+        .then((editProduct) => {
+            const { _id, pname, pprice, type, pquan, isBest, pdet, productPic } = editProduct; // destructing object
+            res.render("productEdit", {
+                _id,
+                pname,
+                pprice,
+                type,
+                pquan,
+                isBest,
+                pdet,
+                productPic
+            })
         })
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
-    
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
+
 
 });
 
-router.put("/productEdit/:id",isAuthenticated, checkAdmin,(req,res)=>{
-  
-    const {_id,pname,pprice,type,pquan,isBest,pdet} = req.body;
-     const toBeUpdate = 
-     {
+router.put("/productEdit/:id", isAuthenticated, checkAdmin, (req, res) => {
+
+    const { _id, pname, pprice, type, pquan, isBest, pdet, productPic } = req.body;
+
+    const toBeUpdate =
+    {
         id: _id,
         pname: pname,
         pprice: pprice,
         type: type,
         pquan: pquan,
         isBest: isBest,
-        pdet: pdet
-     };
-     addition.updateOne({_id: req.params.id},toBeUpdate)
-     .then(()=>{
-         res.redirect("/productDash");
-     })
-     .catch(err=>console.log(`Error happended when updating data from database ${err}`))
-    });
+        pdet: pdet,
+        productPic: productPic
+    };
 
-router.delete("/productDash/:id",isAuthenticated, checkAdmin,(req,res)=>{
-        addition.deleteOne({_id: req.params.id})
-        .then(()=>{
+    addition.updateOne({ _id: req.params.id }, toBeUpdate)
+        .then(() => {
+            addition.findOne({ _id: req.params.id })
+                .then((pic) => {
+                    req.files.productPic.name = `pro_pic_${pic._id}${path.parse(req.files.productPic.name).ext}`;
+                    req.files.productPic.mv(`public/uploads/${req.files.productPic.name}`)
+                        .then(() => {
+                            addition.updateOne({ _id: pic._id }, {
+                                productPic: req.files.productPic.name
+                            })
+                                .then(() => {
+                                    res.redirect("/productDash");
+                                })
+
+                        })
+                });
+        }).catch(err => console.log(`Error happended when inserting data into database ${err}`))
+});
+
+router.delete("/productDash/:id", isAuthenticated, checkAdmin, (req, res) => {
+    addition.deleteOne({ _id: req.params.id })
+        .then(() => {
             res.redirect("/productDash");
         })
-        .catch(err=>console.log(`Error happended when deleting data from database ${err}`))
-    
-    });
+        .catch(err => console.log(`Error happended when deleting data from database ${err}`))
 
-router.get("/product_detail/:id",(req, res) => {
+});
+
+router.get("/product_detail/:id", (req, res) => {
 
     addition.findById(req.params.id) // return an array when using find() method . use the find when you want to pull mutiple values from database 
-    .then((editProduct)=>{
-        const {_id,pname,pprice,type,pquan,isBest,pdet,productPic} = editProduct; // destructing object
-        res.render("product_detail",{
-            _id,
-            pname,
-            pprice,
-            type,
-            pquan,
-            isBest,
-            pdet,
-            productPic
+        .then((editProduct) => {
+            const { _id, pname, pprice, type, pquan, isBest, pdet, productPic } = editProduct; // destructing object
+            res.render("product_detail", {
+                _id,
+                pname,
+                pprice,
+                type,
+                pquan,
+                isBest,
+                pdet,
+                productPic
+            })
         })
-    })
-    .catch(err=>console.log(`Error happended when pulling  from database ${err}`))
+        .catch(err => console.log(`Error happended when pulling  from database ${err}`))
 });
-    
+
 
 module.exports = router;
