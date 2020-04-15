5016ea20c1a6d708a7ffd78ec90bcf2685b2783c
Minsu Kim <51520671+mkim219@users.noreply.github.com>
2020-04-15 08:33:00 -0400

    name change

--- index.js
+++ index.js
@@ -43,15 +43,6 @@ app.use((req,res,next)=>{
 
 app.use(fileUpload());
 
-// app.use((req,res,next)=>{
-//     if(req.body.type == 'admin'){
-//         return true;
-//     }else if(req.body.type == 'user'){
-//         return false;
-//     }
-//     next()
-// });
-
 app.use(session({secret: `${process.env.SESSION_SECRET}`, 
                 resave: false,
                 saveUninitialized: true}));
