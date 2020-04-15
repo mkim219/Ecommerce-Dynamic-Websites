7d4d0e2057d06c6cc0ac20a05d297fd863cbccdc
Minsu Kim <51520671+mkim219@users.noreply.github.com>
2020-04-14 22:26:05 -0400

    bycrpt version changed

--- controllers/customer-manage.js
+++ controllers/customer-manage.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const register = require("../model/customer-model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const isAuthenticated = require('../middleware/auth');
 const checkAdmin = require('../middleware/authorization');
 
